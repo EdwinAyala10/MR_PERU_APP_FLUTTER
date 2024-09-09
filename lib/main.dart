9226751dd99855ad8207c50adf49467b7b35c5aa
BryanAriasC <bariasinv@gmail.com>
2024-09-09 15:31:44 +0200

    descomentar notificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:crm_app/features/shared/presentation/providers/notifications_provider.dart';
 import 'package:crm_app/local_notifications/local_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'config/config.dart';
 import 'package:flutter/material.dart';
@@ -8,9 +10,8 @@ import 'package:intl/date_symbol_data_local.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  // FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
-  // await NotificationsNotifier.initialFCM();
-  // TODO: DESCOMENTAR NOTIFICACIONES PUSH
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  await NotificationsNotifier.initialFCM();
   await LocalNotifications.initializeLocalNotifications();
 
   await Environment.initEnvironment();
